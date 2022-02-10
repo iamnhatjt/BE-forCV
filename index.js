8ab621ed49f0520780562da815d6cdead69d2436
NhatJt vn <iamnhatjt@gmail.com>
2022-02-11 01:46:05 +0700

    nono

--- index.js
+++ index.js
@@ -9,13 +9,28 @@ const loginAR = require("./src/router/loginAR");
 // middle ware
 
 var corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "*",
   credentials: true,
 };
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 // router
+
+app.get("/", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true); // If needed
+
+  res.send("cors problem fixed:)");
+});
 
 app.use("/", loginAR);
 // connect database and start server
